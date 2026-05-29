bbadfeeb534325442f13b020755546f964cdc8ef
ashraful003 <ashraful1510178@gmail.com>
2026-05-30 00:32:50 +0600

    Developed Prescription, Test, Doctor Activity

--- src/main/resources/db/doses.sql
+++ src/main/resources/db/doses.sql
@@ -3,7 +3,5 @@ CREATE TABLE doses (
     dose VARCHAR(255) NOT NULL,
     national_id VARCHAR(50) NOT NULL
 );
-
--- Optional index for fast lookup
 CREATE INDEX idx_doses_national_id ON doses(national_id);
 CREATE INDEX idx_doses_dose ON doses(dose);
